327627f9cecda04f3869ddd15ffe42802cab78be
Zoomingforthagays <nagyhuszarvirag@gmail.com>
2026-02-01 00:44:04 +0100

    Optimalizálás és beállítások előkészítése

--- frontend/javascript/options.js
+++ frontend/javascript/options.js
@@ -1,1 +1,5 @@
-console.log("sikeres betöltés 1");
+//alapértelmezett dolgok
+let volume = 0.5;
+let nyelv = "hungarian"; 
+export {nyelv,volume}; 
+//Ezeket az adatokat majd a bejelentkezest usernél az elmentett cuccaikat kell betölteni => új tábla az SQL-ben
--- frontend/javascript/start_game.js
+++ frontend/javascript/start_game.js
@@ -1,2 +1,7 @@
-console.log("sikeres betöltés 0");
 //Meg kell csinálni az SQL-t, mert addig nem tudok itt haladni
+import { oldalTakarito, fecthData, masikJSMeghivasa  } from "./index.js";
+import { nyelv } from "./options.js";
+
+export async function startGame() {
+  console.log("Játék indítása");
+  oldalTakarito()};
