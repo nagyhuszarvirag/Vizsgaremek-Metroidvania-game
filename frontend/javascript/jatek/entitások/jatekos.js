37a6c2cd6864b87a2b5ded1ee96fdcb6b59fd5f3
Hitechun <imre.huszar6@gmail.com>
2026-03-05 09:32:22 +0100

    issuek kezelése
    megcsináltam hogy a main menüben a háttér kicsit halványabb legyen, hogy a szöveg is látszódjon
    beméreteztem a player collision boxát, hogy ne legyen nagyobb
    csináltam láthatatlan falat a kezdőszoba köré hogy ne lehessen most kiesni

--- frontend/javascript/jatek/entitások/jatekos.js
+++ frontend/javascript/jatek/entitások/jatekos.js
@@ -1,57 +1,63 @@
 export async function jatekos_betolt(k, xpos, ypos) {
-    const player=k.add([
-        sprite("player"),
-        anchor("center"),
-		pos(xpos, ypos),
-        body(),
-        area(),
+    //k.setGravity(800);
+
+    const player = k.add([
+        k.sprite("player"),
+        k.pos(xpos, ypos),
+        k.anchor("center"),
+        k.area({
+            shape: new k.Rect(k.vec2(0, 1), 35, 32) //itt tudod állítgatni a boxát a vec2 az a box pozíciója a másik két szám pedig a szélesség magasság
+        }),
+        k.body(),
+        "player",
     ]);
 
-    await camPos(xpos, ypos);
-    
+    k.camPos(player.pos);
+
     player.onUpdate(() => {
         k.camPos(player.pos);
 
-        if (player.isGrounded()) {
+        /*if (player.isGrounded()) {
             k.setGravity(0);
-		}
-    }); 
+        }*/
+    });
 
     k.debug.inspect = true //Ezt a kettőt majd ki kell kapcsolni, ha kész a játék, de most jól jön a teszteléshez
     k.debug.drawArea = true
 
 
-    const SPEED=120;
-    const JUMP_FORCE=400;
+    const SPEED = 120;
+    const JUMP_FORCE = 400;
 
     //A billenytűket majd dinamikusan kell kezelni.
     //Fine tuningolni kell a sebességet
 
     k.onKeyDown("right", () => {
-	player.flipX = true
-	player.move(SPEED, 0)
-    })
+        player.flipX = true
+        player.move(SPEED, 0)
+    });
 
     k.onKeyDown("left", () => {
-	player.flipX = false
-	player.move(-SPEED, 0)
-    })
+        player.flipX = false
+        player.move(-SPEED, 0)
+    });
 
-    k.onKeyDown("space", () => { 
-	if (player.isGrounded()) {
-        k.setGravity(800);
-		player.jump(JUMP_FORCE)
+    k.onKeyDown("space", () => {
+        if (player.isGrounded()) {
+            player.jump(JUMP_FORCE)
 
-		}
-    })
+        }
+    });
+
+    return player;
 
     /*
     //Ezt majd a kötélmászásnál lesz jó, másképp le kéne tiltani a gombot, amikor nem lehet használni
     k.onKeyDown("up", () => { 
-	player.move(0, -SPEED)
+    player.move(0, -SPEED)
     })
 
     onKeyDown("down", () => {
-	player.move(0, SPEED)
+    player.move(0, SPEED)
     })*/
 }
--- frontend/javascript/jatek/szobak/Kezdoszoba.js
+++ frontend/javascript/jatek/szobak/Kezdoszoba.js
@@ -1,18 +1,67 @@
-import {setBackgroundColor, MapColliderek} from "./Szobakezelo.js";
-import {fecthData} from "../../index.js";
-import {jatekos_betolt} from "../entitások/jatekos.js";
+import { setBackgroundColor, MapColliderek } from "./Szobakezelo.js";
+import { fecthData } from "../../index.js";
+import { jatekos_betolt } from "../entitások/jatekos.js";
 
 export async function Kezdoszoba(k) {
-    setBackgroundColor(k, "#000000");
-    const kezdoszoba_data=await fecthData("http://127.0.0.1:3000/api/map_data/kezdomap.json");
-    
-    const szoba_layerek=kezdoszoba_data.data.layers;
-    const map = k.add([k.pos(0, 0), k.sprite("Kezdoszoba")]);
+  setBackgroundColor(k, "#000000");
 
-    MapColliderek(k, map, szoba_layerek[1].objects);
+  const kezdoszoba_data = await fecthData("http://127.0.0.1:3000/api/map_data/kezdomap.json");
 
-    let xpos=191; //ezt majd később rendesen le kell kezelni vagy valami, ez a savepoint_1 kezdopontjai
-    let ypos=566; 
+  let xpos = 191; //ezt majd később rendesen le kell kezelni vagy valami, ez a savepoint_1 kezdopontjai
+  let ypos = 566;
 
-    jatekos_betolt(k, xpos, ypos); 
+  /*k.camPos(xpos, ypos);
+  k.camScale(3);*/
+
+  const szoba_layerek = kezdoszoba_data.data.layers;
+  const map = k.add([k.pos(0, 0), k.sprite("Kezdoszoba")]);
+
+  MapColliderek(k, map, szoba_layerek[1].objects);
+
+  const mapW = 30 * 32;
+  const mapH = 20 * 32;
+
+  const T = 32;
+
+  //baloldali fal
+  map.add([
+    k.pos(-T, 0),
+    k.rect(T, mapH),
+    k.area(),
+    k.body({ isStatic: true }),
+    k.opacity(0),
+    "Solid",
+  ]);
+
+  //jobboldali fal
+  map.add([
+    k.pos(mapW, 0),
+    k.rect(T, mapH),
+    k.area(),
+    k.body({ isStatic: true }),
+    k.opacity(0),
+    "Solid",
+  ]);
+
+  //alsó fal
+  map.add([
+    k.pos(0, mapH),
+    k.rect(mapW, T),
+    k.area(),
+    k.body({ isStatic: true }),
+    k.opacity(0),
+    "Solid",
+  ]);
+
+  //felső fal
+  map.add([
+    k.pos(0, -T),
+    k.rect(mapW, T),
+    k.area(),
+    k.body({ isStatic: true }),
+    k.opacity(0),
+    "Solid",
+  ]);
+
+  jatekos_betolt(k, xpos, ypos);
 }
