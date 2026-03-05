3b8e635579eededb26a14ebebb341cb946e14c96
nagyhuszarvirag <nagyhuszarvirag@gmail.com>
2026-03-05 10:30:29 +0100

    jatekos_meret_kezeles

--- frontend/javascript/jatek/entitások/jatekos.js
+++ frontend/javascript/jatek/entitások/jatekos.js
@@ -1,52 +1,53 @@
 export async function jatekos_betolt(k, xpos, ypos) {
-    const player=k.add([
-        sprite("player"),
-        anchor("center"),
-		pos(xpos, ypos),
-        body(),
-        area(),
-    ]);
+  const player = k.add([
+    k.sprite("player"),
+    k.pos(xpos, ypos),
+    k.anchor("center"),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 1), 20, 30), //itt tudod állítgatni a boxát a vec2 az a box pozíciója a másik két szám pedig a szélesség magasság
+    }),
+    k.body(),
+    "player",
+  ]);
 
-    k.camPos(xpos, ypos);
-    k.camScale(3);
-    
-    player.onUpdate(() => {
-        k.camPos(player.pos);
+  k.camPos(xpos, ypos);
+  k.camScale(3);
 
-        if (player.isGrounded()) {
-            k.setGravity(0);
-		}
-    }); 
+  player.onUpdate(() => {
+    k.camPos(player.pos);
 
-    k.debug.inspect = true //Ezt a kettőt majd ki kell kapcsolni, ha kész a játék, de most jól jön a teszteléshez
-    k.debug.drawArea = true
+    if (player.isGrounded()) {
+      k.setGravity(0);
+    }
+  });
 
+  k.debug.inspect = true; //Ezt a kettőt majd ki kell kapcsolni, ha kész a játék, de most jól jön a teszteléshez
+  k.debug.drawArea = true;
 
-    const SPEED=120; //Ezt is lehet JSON-ben tárolni security miatt
-    const JUMP_FORCE=400;
+  const SPEED = 120; //Ezt is lehet JSON-ben tárolni security miatt
+  const JUMP_FORCE = 400;
 
-    //A billenytűket majd dinamikusan kell kezelni.
-    //Fine tuningolni kell a sebességet
+  //A billenytűket majd dinamikusan kell kezelni.
+  //Fine tuningolni kell a sebességet
 
-    k.onKeyDown("right", () => {
-	player.flipX = true
-	player.move(SPEED, 0)
-    })
+  k.onKeyDown("right", () => {
+    player.flipX = true;
+    player.move(SPEED, 0);
+  });
 
-    k.onKeyDown("left", () => {
-	player.flipX = false
-	player.move(-SPEED, 0)
-    })
+  k.onKeyDown("left", () => {
+    player.flipX = false;
+    player.move(-SPEED, 0);
+  });
 
-    k.onKeyDown("space", () => { 
-	if (player.isGrounded()) {
-        k.setGravity(800);
-		player.jump(JUMP_FORCE)
+  k.onKeyDown("space", () => {
+    if (player.isGrounded()) {
+      k.setGravity(800);
+      player.jump(JUMP_FORCE);
+    }
+  });
 
-		}
-    })
-
-    /*
+  /*
     //Ezt majd a kötélmászásnál lesz jó, másképp le kéne tiltani a gombot, amikor nem lehet használni
     k.onKeyDown("up", () => { 
 	player.move(0, -SPEED)
