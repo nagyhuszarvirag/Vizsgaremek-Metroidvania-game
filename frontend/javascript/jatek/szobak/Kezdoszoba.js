cf29e6086869494579b90b284a40c97737312457
Zoomingforthagays <nagyhuszarvirag@gmail.com>
2026-03-28 20:48:36 +0100

    Mitteous map alap és társai

--- frontend/javascript/jatek/szobak/Kezdoszoba.js
+++ frontend/javascript/jatek/szobak/Kezdoszoba.js
@@ -2,79 +2,38 @@ import {
   setBackgroundColor,
   MapColliderek,
   NPCCollider,
+  SzobakiesesKezelo,
+  SzobavaltozatoKezelo
 } from "./Szobakezelo.js";
 import { fecthData } from "../../index.js";
 import { jatekos_betolt } from "../entitások/jatekos.js";
 
-export async function Kezdoszoba(k) {
-  setBackgroundColor(k, "#000000");
+export async function Kezdoszoba(k,  szoba_belepesi_pont = null) {
+  setBackgroundColor(k, "#00001b");
+
+  //map méret pixelben
+  const mapW = 32 * 60;
+  const mapH = 32 * 50;
 
   const kezdoszoba_data = await fecthData(
     "http://127.0.0.1:3000/api/map_data/kezdomap.json",
   );
 
-  let xpos = 191; //ezt majd később rendesen le kell kezelni vagy valami, ez a savepoint_1 kezdopontjai
-  let ypos = 566;
-
-  /*k.camPos(xpos, ypos);
-    k.camScale(3);*/
+  let xpos = kezdoszoba_data.data.layers[2].objects[0].x;
+  let ypos = kezdoszoba_data.data.layers[2].objects[0].y;
 
   const szoba_layerek = kezdoszoba_data.data.layers;
   const map = k.add([k.pos(0, 0), k.sprite("Kezdoszoba")]);
 
-  MapColliderek(k, map, szoba_layerek[1].objects);
-  NPCCollider(k, szoba_layerek[2].objects, "Prowl");
-
-  //MapColliderek(k, map, szoba_layerek[1].objects);
-
-  //map méret pixelben
-  const mapW = 30 * 32;
-  const mapH = 20 * 32;
-
-  //láthatatlan falak vastagsága
-  const T = 32;
-
-  //bal
-  map.add([
-    k.pos(-T, 0),
-    k.rect(T, mapH),
-    k.area(),
-    k.body({ isStatic: true }),
-    k.opacity(0),
-    "Solid",
-  ]);
-
-  //jobb
-  map.add([
-    k.pos(mapW, 0),
-    k.rect(T, mapH),
-    k.area(),
-    k.body({ isStatic: true }),
-    k.opacity(0),
-    "Solid",
-  ]);
-
-  //alsó
-  map.add([
-    k.pos(0, mapH),
-    k.rect(mapW, T),
-    k.area(),
-    k.body({ isStatic: true }),
-    k.opacity(0),
-    "Solid",
-  ]);
-
-  //felső
-  map.add([
-    k.pos(0, -T),
-    k.rect(mapW, T),
-    k.area(),
-    k.body({ isStatic: true }),
-    k.opacity(0),
-    "Solid",
-  ]);
+  MapColliderek(k, map, szoba_layerek[3].objects);
+  NPCCollider(k, szoba_layerek[4].objects, "Prowl");
 
   const player = jatekos_betolt(k, xpos, ypos);
+  
+  SzobakiesesKezelo(k, map, mapW, mapH);
+  SzobavaltozatoKezelo(k, kezdoszoba_data.data.layers[5].objects[0].x, kezdoszoba_data.data.layers[5].objects[0].y, kezdoszoba_data.data.layers[5].objects[0].width, kezdoszoba_data.data.layers[5].objects[0].height, "Mitteous_Plateau");
+
+/*
 
   //átjáró zóna
   k.add([
@@ -88,5 +47,5 @@ export async function Kezdoszoba(k) {
   k.onCollide("player", "atjaro", () => {
     console.log("váltás");
     k.go("mitteous");
-  });
+  });*/
 }
